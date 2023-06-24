ac1d615b9d46626a3245a18183ee33545e3f12a4
nagiarjun05 <nagiarjun05@gmail.com>
2023-06-24 16:21:23 +0530

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+    <div>
+        <div><h1>Expense Items</h1></div>
+        <div><h2>Food Rs 100.00/-</h2></div>
+        <div><h2>Petrol Rs 100.00/-</h2></div>
+        <div><h2>Movies Rs 200.00/-</h2></div>
+    </div>
+    )
+}
+
+export default ExpenseItem;
