63be205cb69ad4a9d862ee69dc4f342526a7b90b
nagiarjun05 <nagiarjun05@gmail.com>
2023-06-27 17:40:36 +0530

    Amount Button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem=(data)=>{
+const ExpenseItem=(prop)=>{
+    let [amount,setAmount]=useState(prop.amount)
+    // let count=0
+    const clickHandler = () => {
+        setAmount('100$')
+        console.log(amount)
+    };
+    // const countInc = () => {
+    //     count++
+    //     document.getElementById('count').innerHTML=count
+    //     console.log(count)
+    // };
     return (
     <Card className='expense-item'>
-        <ExpenseDate date={data.date}/>
-        <ExpenseDetails amount={data.amount}  location={data.location}   title={data.title} />
+        <ExpenseDate date={prop.date}/>
+        <ExpenseDetails amount={amount}  location={prop.location}   title={prop.title} />
+        <button onClick={clickHandler}>Update Amount</button>
+        {/* <button id='count' onClick={countInc}>{count}</button> */}
     </Card>
     )
 }
